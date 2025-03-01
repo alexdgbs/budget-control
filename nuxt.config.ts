cd945a560b9a7cb341cd3e2c7d709b01df690f74
alexdgbs <alexdbc24@gmail.com>
2025-02-28 18:44:34 -0600

    icon app update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,23 @@
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+  app: {
+    head: {
+      title: "Budget Control",
+      meta: [
+        { name: "description", content: "Budget" }
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/koala.png" }
+      ]
+    }
+  }
 });
